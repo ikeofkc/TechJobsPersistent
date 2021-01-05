2dedfd802dcbc44400fefb893fe47a46b4d6211e
Ike AhLoe <ikeahloe@gmail.com>
2021-01-05 03:49:37 -0600

    Completed part 3

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,24 @@
 --Part 1
 
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'techjobs'
+AND TABLE_NAME = 'Jobs';
+--OR
+DESCRIBE Jobs;
+--OR
+SHOW COLUMNS FROM techjobs.jobs;
+
+
 --Part 2
+
+SELECT Names
+FROM Employers
+WHERE Location = "St. Louis City";
 
 --Part 3
 
+SELECT Name, Description
+FROM Skills
+INNER JOIN JobSkills ON Skills.Id = JobSkills.SkillId
+ORDER BY Name ASC;

9d5a2953b73f81fe372d1840c533bcd10a07dbdf
Ike AhLoe <ikeahloe@gmail.com>
2021-01-07 09:03:15 -0600

    cleanup

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,24 +1,18 @@
 --Part 1
-
-SELECT COLUMN_NAME, DATA_TYPE
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'techjobs'
-  AND TABLE_NAME = 'Jobs';
---OR
-DESCRIBE Jobs;
---OR
-SHOW COLUMNS FROM techjobs.jobs;
+int Id
+varchar Name
+int EmployerId
 
 
 --Part 2
 
 SELECT Names
 FROM Employers
-WHERE Location = "Kansas City";
+WHERE Location = "St. Louis City";
 
 --Part 3
 
 SELECT Name, Description
 FROM Skills
-         INNER JOIN JobSkills ON Skills.Id = JobSkills.SkillId
-ORDER BY Name ASC;
+INNER JOIN JobSkills ON Skills.Id = JobSkills.SkillId
+ORDER BY Name ASC, Description ASC;
